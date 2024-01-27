b21ddb726c58b7769e82707e5fcce1dd74e25a18
Jewel Jo Prince <joprince@Jewels-MacBook-Pro.local>
2024-01-27 16:02:04 +0000

    Update dependencies to python3

--- etcher-build/old_build_method/install_dependencies.sh
+++ etcher-build/old_build_method/install_dependencies.sh
@@ -6,7 +6,7 @@ echo
 sudo apt-get update
 
 # install build dependencies
-sudo apt-get install -y git python gcc g++ make libx11-dev libxkbfile-dev fakeroot rpm libsecret-1-dev python2.7-dev python-pip python-setuptools jq curl libudev-dev
+sudo apt-get install -y git python3 gcc g++ make libx11-dev libxkbfile-dev fakeroot rpm libsecret-1-dev python3-dev python3-pip python3-setuptools jq curl libudev-dev
 
 # install Node.JS - version >= 10.15.1, < 11.0.0
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
